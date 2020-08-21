4572930a515dbb21cf855c45be0ebe7029cc4a39
410406130@qq.com <410406130@qq.com>
2020-08-21 18:34:44 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ export default {
     async asyncData({$content, params}){
         const docs = await $content(params.pathMatch || 'index').fetch()
 
-
+        console.log(docs);
         return {docs}
     }
 }
