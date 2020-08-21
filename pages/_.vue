a33aae93f5d3c983647ae931f3ba26349cea6a75
410406130@qq.com <410406130@qq.com>
2020-08-21 16:59:06 +0800

    1

---
+++ pages/_.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        22222
+    </div>
+</template>
+<script>
+export default {
+    // async asyncData({$content, params}){
+    //     const docs = await $content(params.pathMatch).fetch()
+
+
+    //     return {docs}
+    // }
+}
+</script>
+<style scoped>
+    div{
+        color: #fff;
+        background-color: red;
+    }
+</style>
